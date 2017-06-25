d31f1171045dd2abbb2355d446f030c7e60e121f
Jon <jon_rosenberg@hotmail.com>
2017-06-25 00:48:15 -0700

    Adding loop to open a socket to an endpoint and issue an HTTP request

--- samples/scratch/sock.c
+++ samples/scratch/sock.c
@@ -8,49 +8,58 @@ int main(int argc, char **argv)
 {
     while (1)
     {
-    	int s = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    	int s = socket(AF_INET, SOCK_STREAM, 6);
     	struct sockaddr_in server;
     	char* message;
     	char reply[16 * 1024];
+
+        server.sin_addr.s_addr = inet_addr("13.107.245.94");
+        server.sin_family = AF_INET;
+        server.sin_port = htons(80);
+
+        message = "GET /id HTTP/1.1\r\nHost: failhttp.afdorigin-test.com\r\n\r\n";
+
+        bzero(reply, sizeof(reply));
     	
-    	if (s == -1)
+    	if (s != -1)
+    	{
+            int connectResult = connect(s, (struct sockaddr *)&server, sizeof(server));
+            printf("connectResult: %d\n", connectResult);
+            if (connectResult >= 0)
+            {
+                int sendResult = send(s, message, strlen(message), 0);
+                printf("sendResult: %d\n", sendResult);
+                if (sendResult >= 0)
+                {
+                    int recvResult = recv(s, reply, sizeof(reply), 0);
+                    printf("recvResult: %d\n", recvResult);
+                    if (recvResult >= 0)
+                    {
+                        printf("received:  %s\n\n", reply);
+                    }
+                    else
+                    {
+                        printf("Recv failed\n");
+                    }
+                }
+                else
+                {
+                    printf("Send failed\n");
+                }
+            }
+            else
+            {
+                printf("Connection error: %d\n", connectResult);
+            }
+            
+            close(s);
+    	}
+        else
     	{
     		printf("Could not create socket.\n");
     	}
     	
-    	server.sin_addr.s_addr = inet_addr("131.253.33.200");
-    	server.sin_family = AF_INET;
-    	server.sin_port = htons(80);
-    	
-    	if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
-    	{
-    		printf("Connection error\n");
-    		return 1;
-    	}
-    	
-    	printf("Connected!\n");
-    	
-    	message = "GET /fdv2/diagnostics.aspx HTTP/1.1\r\nHost: www.bing.com\r\n\r\n";
-    	if (send(s, message, strlen(message), 0) < 0)
-    	{
-    		printf("Send failed\n");
-    		return 1;
-    	}
-    	
-    	printf("Data sent!\n");
-    	
-    	if (recv(s, reply, sizeof(reply), 0) < 0)
-    	{
-    		printf("Recv failed\n");
-    		return 1;
-    	}
-    	
-    	printf("Reply received!\n");
-    	printf("%s\n", reply);
-    	
-    	close(s);
-
-        sleep(2);
+        sleep(1);
     }
 	
 	return 0;
